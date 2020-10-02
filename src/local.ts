2192ed6b29c7db5cc0a94f32843364de3a33c529
yihaimen <yihaimen@outlook.com>
2020-10-02 22:17:01 +0800

    add: two demos

---
+++ src/local.ts
@@ -0,0 +1,7 @@
+import { add, multiply } from './arithmetic.ts' 
+
+function totalCost(outBound:number, inBound:number, tax:number): number {
+  return multiply(add(outBound, inBound), tax)
+}
+
+console.log(totalCost(19, 31, 1.2));
