74b71fcc038345848c95d55f6e173ae8a288837d
yihaimen <yihaimen@outlook.com>
2020-10-06 23:19:33 +0800

    init project

---
+++ deps.ts
@@ -0,0 +1,12 @@
+export {
+  Application,
+  Router,
+  Response,
+  Status,
+  Request,
+  RouteParams,
+  Context,
+  RouterContext,
+  helpers,
+  send,
+} from "https://deno.land/x/oak/mod.ts";
--- src/fetch.ts
+++
@@ -1,6 +0,0 @@
-const json = fetch("https://api.github.com/users/denoland");
-json.then((res) => res.json()).then((data) => {
-  console.log("jsonData", data);
-}).catch((err) => {
-  console.log("err", err);
-});
