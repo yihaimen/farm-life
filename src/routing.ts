e27b479a60a2bdb9c3cf626015eb4a85cf76f22b
yihaimen <yihaimen@outlook.com>
2020-10-11 22:42:47 +0800

    add: createTodo

--- src/routing.ts
+++ src/routing.ts
@@ -1,8 +1,10 @@
 import { Router } from "../deps.ts";
 import getTodos from "./handlers/getTodos.ts";
+import createTodo from "./handlers/createTodo.ts";
 
 const router = new Router();
 
-router.get("/todos", getTodos);
+router.get("/todos", getTodos)
+  .post("/todos", createTodo);
 
 export default router;
--- src/services/todos.ts
+++ src/services/todos.ts
@@ -8,3 +8,16 @@ export const getTodos = async (): Promise<Todo[]> => {
   const todos = await fetchData();
   return todos.sort((a, b) => a.title.localeCompare(b.title));
 };
+
+export const createTodo = async (todoData: TodoData): Promise<string> => {
+  const todos = await fetchData();
+
+  const newTodo = {
+    ...todoData,
+    id: createId(),
+  };
+
+  await persistData([...todos, newTodo]);
+
+  return newTodo.id;
+};
