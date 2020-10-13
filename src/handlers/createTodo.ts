f5f109a1d0885e5567027646e178ff9bac70e888
yihaimen <yihaimen@outlook.com>
2020-10-13 21:58:50 +0800

    fix(create): need await body value

--- src/handlers/createTodo.ts
+++ src/handlers/createTodo.ts
@@ -10,35 +10,18 @@ export default async (
     return;
   }
 
-  // const {
-  //   value: { userId, title, completed = false },
-  // } = await request.body();
+  const body = request.body();
+  const { userId, title } = await body.value;
 
-  // if (!userId || !title) {
-  //   response.status = 422;
-  //   response.body = {
-  //     msg: "Incorrect todo data. userId and title are required",
-  //   };
-  //   return;
-  // }
+  if (!userId || !title) {
+    response.status = 422;
+    response.body = {
+      msg: "Incorrect todo data. userId and title are required",
+    };
+    return;
+  }
 
-  // const todoId = await createTodo({ userId, title, completed });
+  const todoId = await createTodo({ userId, title, completed: false });
 
-  // response.body = { msg: "Todo created", todoId };
-
-  const res = await request.body();
-  response.body = res;
+  response.body = { msg: "Todo created", todoId };
 };
-
-// import InvalidedParamsException from "../exception/InvalidedParamsException.ts";
-// export const add = async (
-//   { request, response }: { request: Request; response: Response },
-// ) => {
-//   const body = request.body();
-//   const values = await body.value;
-//   if (!values.userId || !values.title) {
-//     throw new InvalidedParamsException("userId and title must be supplied");
-//   }
-//   await addTodo(values.userId, values.title);
-//   response.status = 200;
-// };
